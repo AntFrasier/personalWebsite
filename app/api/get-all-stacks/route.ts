16e7e8c522abb0f3f4401d8ad748818825794afe
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2024-05-14 19:38:39 +0200

    Modified get all stacks handler returned type

--- app/api/get-all-stacks/route.ts
+++ app/api/get-all-stacks/route.ts
@@ -2,22 +2,16 @@ import getAllStacks from '@/lib/getAllStacks';
 import prisma from '../../../lib/prisma';
 import { NextRequest } from 'next/server';
 
-async function handler(req:NextRequest) {
+async function handler(req:NextRequest) :   Promise<never[] | Response>{ //added return typed to avoid vercel error
 
-    if (req.method == 'POST') {
-        console.log("POST METHOD FOR images");
-        return [];
-    }
-    else {
         try {
             const result = await getAllStacks();
-            return Response.json({body : result})
+                return Response.json({body : result})
         }
         catch (err) {
             console.log(err);
             return Response.json({status:500, error: err})
         }
     }
-    }
 
-export { handler as GET, handler as POST}
+export { handler as GET}
