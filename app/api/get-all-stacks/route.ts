426f6a7b0ba8659abb3d5a44d9e040e13daa094a
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2024-05-14 19:49:31 +0200

    updated nextjs 13 -> 14

--- app/api/get-all-stacks/route.ts
+++ app/api/get-all-stacks/route.ts
@@ -1,8 +1,6 @@
 import getAllStacks from '@/lib/getAllStacks';
-import prisma from '../../../lib/prisma';
-import { NextRequest } from 'next/server';
 
-async function handler(req:NextRequest) :   Promise<never[] | Response>{ //added return typed to avoid vercel error
+async function handler() :   Promise<never[] | Response>{ //added return typed to avoid vercel error
 
         try {
             const result = await getAllStacks();
