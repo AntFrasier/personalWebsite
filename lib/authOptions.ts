d1f99d2a260e194fb911e7fa49dceb68f139342e
CMaranber <84241354+AntFrasier@users.noreply.github.com>
2024-05-14 20:03:10 +0200

    try to fix auth route type error

---
+++ lib/authOptions.ts
@@ -0,0 +1,60 @@
+import GithubProvider, { GithubProfile } from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/prisma"
+import { Adapter } from "next-auth/adapters";
+import { NextAuthOptions } from "next-auth";
+const githubtId = process.env.GITHUB_CLIENT_ID as string
+const githubtSecret = process.env.GITHUB_SECRET as string
+
+
+// if (!githubtId || !githubtSecret) {
+//     throw new Error ('git hub cred missing')
+// }
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+        profile(profile : GithubProfile) {
+            return {
+                // ...profile,
+                id: profile.id.toString(),
+                role: "user",
+                image: profile.avatar_url,
+                name:profile.name,
+                email:profile.email
+            }
+        },
+        clientId: githubtId,
+        clientSecret: githubtSecret,
+    }),
+    ],
+    adapter: PrismaAdapter(prisma) as Adapter,
+    session:{
+        strategy:"jwt"
+    },
+    callbacks: {
+       
+        async jwt ({token, user}) {
+            if (user) token.role = user.role;
+            return token
+        },
+        session({ session, token }) {
+            return { ...session,
+              user: { ...session.user,
+                role: token.role,
+              }
+            }
+          },
+         async signIn({ user }) {
+             const isAllowedToSignIn = user.role == 'admin' || user.email == "frasier@hotmail.fr" ? true : false; //not secure at all
+            if (isAllowedToSignIn) {
+                return true
+            } else {
+              return '/unauthorized'
+
+            }
+          },
+
+
+    }
+} satisfies NextAuthOptions;
